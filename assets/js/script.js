efe1af4ac295c56d95f773bd282f75d3b80084a3
caodeptrai <duongvancao999@gmail.com>
2022-06-23 01:10:15 +0700

    v12

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,71 @@
 $(document).ready(function($){
 
     /**
-     * lam cho thang search 
-     * khi click vao no thi chay o day
+     --------------------------Search---------------------
      */
     $('body').on('click',(e)=>{
         var width = $(window).width();
-        if (width < 1300){
-            $(".search-btn").click(function(){
-                $(".search-btn").addClass("js-search__btn--click");
-                $(".header-navbar__item--more").addClass("navbar__item--hide");
-                $(".header-search").addClass("js-header-search");
-                $(".header-search__input").addClass("js-header-search__input");
-            })
-            $(".js-app").click(function(){
-                
-            })
-        }else{
-           
+
+        $(".search-btn").click(function(){
+          $(".search-btn").addClass("js-search__btn--click");
+          $(".header-search").addClass("js-header-search");
+          $(".header-search__input").addClass("js-header-search__input");
+          $(".tes").addClass("overlay");
+          if(width < 1300) {
+            $(".header-navbar__item--more").addClass("navbar__item--hide");
+
+          }else {
+            
+            $(".header-navbar__item--more").addClass("navbar__item--show");
+            $(".header-navbar__item--res").addClass("navbar__item--hide");
+            
+          }
+          
+        });
+
+      $(".tes").click(function(){
+        $(".search-btn").removeClass("js-search__btn--click");
+        $(".header-search").removeClass("js-header-search");
+        $(".header-search__input").removeClass("js-header-search__input");
+        if(width < 1300) {
+          $(".header-navbar__item--more").removeClass("navbar__item--hide");
+
+        }else {
+          $(".header-navbar__item--more").removeClass("navbar__item--show");
+          $(".header-navbar__item--res").removeClass("navbar__item--hide");
         }
+        
+      });
+
+
+
+        
 
     });
 
+    /*
+        -----------------Menu--------------------    
+    */ 
+    $(".js-menu").click(function(){
+        $(".js-modal").addClass("open-menu");
+        $(".js-modal__menu").addClass("show-menu")
+    });
+
+    $(".js-modal").click(function(){
+        $(".js-modal").removeClass("open-menu");
+        $(".js-modal__menu").removeClass("show-menu")
+    });
+
+    $(".js-modal__menu").click(function(event){
+        event.stopPropagation();
+    });
+
+
+
+
+
     /**
-     * 
+     -----------------Slider--------------------------
      */
     $('.multiple-items').slick({
         nav: false,
@@ -61,24 +103,26 @@ $(document).ready(function($){
           ]
 
     });
-    const modal = document.querySelector('.js-modal');
-    const menu = document.querySelector('.js-menu');
-    const menuContainer = document.querySelector('.js-modal__body')
-    function showMenu(){
-        modal.classList.add('open-menu');
-    }
-    function hideMenu(){
-        modal.classList.remove('open-menu');
 
-    }
+    
+    // const modal = document.querySelector('.js-modal');
+    // const menu = document.querySelector('.js-menu');
+    // const menuContainer = document.querySelector('.js-modal__body')
+    // function showMenu(){
+    //     modal.classList.add('open-menu');
+    // }
+    // function hideMenu(){
+    //     modal.classList.remove('open-menu');
+
+    // }
     
 
-    menu.addEventListener('click',showMenu);
+    // menu.addEventListener('click',showMenu);
     
-    modal.addEventListener('click',hideMenu);
+    // modal.addEventListener('click',hideMenu);
     
-    menuContainer.addEventListener('click',function(event){
-        event.stopPropagation();
-    });
+    // menuContainer.addEventListener('click',function(event){
+    //     event.stopPropagation();
+    // });
 
 });
