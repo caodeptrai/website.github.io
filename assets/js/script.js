71113450b06b8c70eb19bac2bd96f03b5cd93374
caodeptrai <duongvancao999@gmail.com>
2022-06-23 11:00:20 +0700

    elgm

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,6 +75,7 @@ $(document).ready(function($){
     /**
      -----------------Slider--------------------------
      */
+    
     $('.multiple-items').slick({
         nav: false,
         dots:true,
