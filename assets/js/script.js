9ec99063d34e062e5c47da862a9038a4c7886cbb
caodeptrai <duongvancao999@gmail.com>
2022-06-23 10:51:19 +0700

    v14

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,7 +81,7 @@ $(document).ready(function($){
         nextArrow: false,
         arrows:false,
         infinite:true,
-        // autoplay: true,
+        autoplay: true,
         autoplaySpeed :1000,
         slidesToShow: 3,
         slidesToScroll: 2,
